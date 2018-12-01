80a42eb185eeda55c0ed9f56c8d917d869ae0e89
Billy Ceskavich <bceskavich@gmail.com>
2018-12-01 09:16:55 -0500

    Initial commit: scaffold

---
+++ lib/advent.ex
@@ -0,0 +1,18 @@
+defmodule Advent do
+  @moduledoc """
+  Documentation for Advent.
+  """
+
+  @doc """
+  Henlo world.
+
+  ## Examples
+
+      iex> Advent.henlo()
+      :world
+
+  """
+  def henlo do
+    :world
+  end
+end
